c691f8e41d920a2fea1294f2a21491d7c3d04996
Yorick <yorick@ctrlaltdev.xyz>
2018-05-20 21:54:10 -0700

    update typo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 600, height: 800})
   
     // and load the index.html of the app.
     win.loadURL(url.format({
@@ -18,7 +18,7 @@ const {app, BrowserWindow} = require('electron')
       slashes: true
     }))
 
-    setTimeout(printResume, 1000)
+    setTimeout(printResume, 3000)
   
     // Emitted when the window is closed.
     win.on('closed', () => {
